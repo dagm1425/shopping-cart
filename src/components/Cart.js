f9d363b0db9ee3d69e63c385a00ce55a52864c72
Dagmawi <dagmawinebeyu3@gmail.com>
2023-03-22 16:28:53 +0300

    Add further styles to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,6 +3,9 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import CartItem from "./CartItem";
+import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineShopping } from "react-icons/ai";
+import { IconContext } from "react-icons";
 
 export default function Cart(props) {
   const { cart, totalPrice, updateQuantity, isCartOpen, closeCart, rmBg } =
@@ -22,23 +25,47 @@ export default function Cart(props) {
   return (
     <>
       <CartWrapper active={isCartOpen}>
-        <button type="button" onClick={closeCart}>
-          Close cart
-        </button>
+        <CloseBtn onClick={closeCart}>
+          <IconContext.Provider
+            value={{
+              style: { fontSize: "26px", color: "#000" },
+            }}
+          >
+            <AiOutlineClose />
+          </IconContext.Provider>
+        </CloseBtn>
         {cart.length ? (
-          <>
-            <ItemsWrapper>{cartItems}</ItemsWrapper>
-            <h3 style={{ textAlign: "center" }}>Subtotal: {totalPrice}</h3>
-          </>
+          <LoadedCartWrapper>
+            <div>
+              <CartItemsWrapper>{cartItems}</CartItemsWrapper>
+              <SubTotWrapper>
+                <SubTotP>Subtotal:</SubTotP>
+                <SubTotSpan>{"$" + totalPrice.toFixed(2)}</SubTotSpan>
+              </SubTotWrapper>
+            </div>
+            <CheckoutBtn>Checkout</CheckoutBtn>
+          </LoadedCartWrapper>
         ) : (
-          <>
+          <EmptyCartWrapper>
             <h3>Your cart is empty.</h3>
-            <button type="button" onClick={closeCart}>
+            <IconContext.Provider
+              value={{
+                style: {
+                  fontSize: "10rem",
+                  color: "#000",
+                  opacity: "0.1",
+                  margin: "0 auto",
+                },
+              }}
+            >
+              <AiOutlineShopping />
+            </IconContext.Provider>
+            <MainBtn onClick={closeCart}>
               <Link to="/shop" onClick={rmBg}>
                 Browse items
               </Link>
-            </button>
-          </>
+            </MainBtn>
+          </EmptyCartWrapper>
         )}
       </CartWrapper>
       <Overlay active={isCartOpen} onClick={closeCart} />
@@ -51,8 +78,9 @@ const CartWrapper = styled.div`
   position: fixed;
   transform: translateX(100%);
   transition: 200ms ease-in-out;
-  width: 25%;
+  width: 35%;
   height: 100vh;
+  padding: 0 4rem;
   top: 0;
   right: 0;
   z-index: 10;
@@ -82,8 +110,99 @@ const Overlay = styled.div`
   pointer-events: all;`}
 `;
 
-const ItemsWrapper = styled.div`
+const CloseBtn = styled.button`
+  position: absolute;
+  top: 6%;
+  right: 15%;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`;
+
+const LoadedCartWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const EmptyCartWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const SubTotWrapper = styled.div`
+  text-align: center;
+  padding-top: 1rem;
+`;
+
+const SubTotP = styled.p`
+  display: inline;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-right: 0.5rem;
+`;
+
+const SubTotSpan = styled.span`
+  font-size: 1.25rem;
+`;
+
+const MainBtn = styled.button`
+  position: relative;
+  font-size: 0.9rem;
+  letter-spacing: 0.05rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  width: 75%;
+  padding: 2rem 0;
+  border: 1px solid #000;
+  outline: none;
+  cursor: pointer;
+  z-index: 1;
+  transition: 200ms ease-in-out;
+
+  & a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  &:hover,
+  &:focus {
+    color: #fff;
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+    background-color: #000;
+    transition: 250ms ease-in-out;
+    transform: scaleX(0);
+    transform-origin: left;
+  }
+
+  &:hover::before,
+  &:focus::before {
+    transform: scaleX(1);
+  }
+`;
+
+const CheckoutBtn = styled(MainBtn)`
+  margin-bottom: 6rem;
+`;
+
+const CartItemsWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+  margin-top: 8rem;
 `;
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import styled from "styled-components";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { IconContext } from "react-icons";
 
 export default function CartItem(props) {
   const { id, title, quantity, img, price, updateQuantity } = props;
@@ -9,22 +11,29 @@ export default function CartItem(props) {
     <ItemWrapper>
       <Img src={img} alt="item-img" />
       <div>
-        <p style={{ fontWeight: "bold" }}>{title}</p>
-        <p>{price}</p>
-        <button
-          type="button"
-          onClick={(e) => updateQuantity(e.target.innerText, id)}
-        >
-          -
-        </button>
-        <span>{quantity}</span>
-        <button
-          type="button"
-          content="+"
-          onClick={(e) => updateQuantity(e.target.innerText, id)}
-        >
-          +
-        </button>
+        <TitleP>{title}</TitleP>
+        <PriceP>{"$" + price.toFixed(2)}</PriceP>
+        <QtyController>
+          <QtyBtn onClick={() => updateQuantity("-", id)}>
+            <IconContext.Provider
+              value={{
+                style: { fontSize: "18px", color: "#000" },
+              }}
+            >
+              <AiOutlineMinus />
+            </IconContext.Provider>
+          </QtyBtn>
+          <QtyP>{quantity}</QtyP>
+          <QtyBtn onClick={() => updateQuantity("+", id)}>
+            <IconContext.Provider
+              value={{
+                style: { fontSize: "18px", color: "#000" },
+              }}
+            >
+              <AiOutlinePlus />
+            </IconContext.Provider>
+          </QtyBtn>
+        </QtyController>
       </div>
     </ItemWrapper>
   );
@@ -34,13 +43,47 @@ const ItemWrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 0.75rem;
-  height: 110px;
-  width: 85%;
+  width: 90%;
   margin: 0 auto;
-  border: 3px solid #fff;
+  padding-bottom: 1.5rem;
+  border-bottom: 1px solid #000;
 `;
 
 const Img = styled.img`
-  height: 75px;
-  width: 35%;
+  height: 110px;
+  width: 110px;
+  object-fit: contain;
+  background-color: #fff;
+`;
+
+const TitleP = styled.p`
+  margin-bottom: 1rem;
+`;
+
+const PriceP = styled.p`
+  font-weight: 700;
+  margin-bottom: 0.75rem;
+`;
+
+const QtyController = styled.div`
+  display: flex;
+  width: fit-content;
+  align-items: center;
+  gap: 8px;
+  padding: 4px;
+  border: 0.75px solid #000;
+`;
+
+const QtyBtn = styled.button`
+  border: none;
+  outline: none;
+  background: transparent;
+  cursor: pointer;
+`;
+
+const QtyP = styled.p`
+  // width: 50px;
+  // height: 28px;
+  // text-align: center;
+  // margin-top: 0.75rem;
 `;
