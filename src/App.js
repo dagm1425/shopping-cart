dbb8374c27893e5a09833f270e20e57c4f370c81
Dagmawi <dagmawinebeyu3@gmail.com>
2023-04-11 20:08:12 +0300

    Add filters

--- src/App.js
+++ src/App.js
@@ -13,14 +13,24 @@ import styled, { css } from "styled-components";
 function App() {
   const [items, setItems] = useState([]);
   const [cart, setCart] = useState([]);
+  const [filters, setFilters] = useState({
+    gender: { man: false, woman: false },
+    brand: { Hanes: false, Champion: false, Under_Armour: false },
+    price: { upTo20: false, from20To25: false, from25To30: false },
+  });
   const [totalPrice, setTotalPrice] = useState(0);
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [selectValue, setSelectValue] = useState("sortDefault");
   const path = useLocation().pathname;
   const location = path.split("/")[1];
 
   useEffect(() => {
     setTotalPrice(computeTotalPrice());
   }, [cart]);
+
+  useEffect(() => {
+    filterItems();
+  }, [filters]);
 
   const loadItems = () => {
     setItems(products);
@@ -64,28 +74,110 @@ function App() {
     setCart(cartUpdate);
   };
 
+  const sortItems = (sorting) => {
+    setSelectValue(sorting);
+
+    sorting === "sortDefault"
+      ? sortDefault()
+      : sorting === "sortPriceLtoH"
+      ? sortPriceLtoH()
+      : sortPriceHtoL();
+  };
+
   const sortDefault = () => {
     setItems(products);
   };
 
   const sortPriceLtoH = () => {
     setItems((prevItems) => {
-      return prevItems.map((el) => el).sort((a, b) => a.price - b.price);
+      return prevItems.slice().sort((a, b) => a.price - b.price);
     });
   };
 
   const sortPriceHtoL = () => {
     setItems((prevItems) => {
-      return prevItems.map((el) => el).sort((a, b) => b.price - a.price);
+      return prevItems.slice().sort((a, b) => b.price - a.price);
     });
   };
 
-  const sortItems = (sorting) => {
-    sorting === "sortDefault"
-      ? sortDefault()
-      : sorting === "sortPriceLtoH"
-      ? sortPriceLtoH()
-      : sortPriceHtoL();
+  const updateFilters = (e) => {
+    const param = e.target.name;
+    const val = e.target.value;
+
+    setFilters((prevFilters) => {
+      return {
+        ...prevFilters,
+        [param]: {
+          ...prevFilters[param],
+          [val]: !prevFilters[param][val],
+        },
+      };
+    });
+  };
+
+  const collectFilters = () => {
+    const { gender, brand, price } = filters;
+    const appliedFilters = {
+      gender: [],
+      brand: [],
+      price: [],
+    };
+
+    for (let genderKey in gender) {
+      if (gender[genderKey]) appliedFilters.gender.push(genderKey);
+    }
+
+    for (let brandKey in brand) {
+      if (brand[brandKey]) appliedFilters.brand.push(brandKey);
+    }
+
+    for (let priceKey in price) {
+      if (price[priceKey]) appliedFilters.price.push(priceKey);
+    }
+
+    return appliedFilters;
+  };
+
+  const filterItems = () => {
+    const filters = collectFilters();
+    const keys = Object.keys(filters);
+
+    const filteredItems = products.filter((product) => {
+      return keys.every((key) => {
+        if (!filters[key].length) return true;
+        if (key === "price") {
+          return filters[key].every((priceKey) => {
+            if (priceKey === "upTo20") return product[key] <= 20;
+            else if (priceKey === "from20To25")
+              return product[key] > 20 && product[key] <= 25;
+            else if (priceKey === "from25To30")
+              return product[key] > 25 && product[key] <= 30;
+          });
+        }
+        return filters[key].includes(product[key]);
+      });
+    });
+
+    setItems(filteredItems);
+    setSelectValue("sortDefault");
+  };
+
+  const resetFilters = () => {
+    const { gender, brand, price } = filters;
+
+    Object.keys(gender).forEach((key) => {
+      gender[key] = false;
+    });
+
+    Object.keys(brand).forEach((key) => {
+      brand[key] = false;
+    });
+
+    Object.keys(price).forEach((key) => {
+      price[key] = false;
+    });
+
+    setFilters({ gender, brand, price });
   };
 
   const openCart = () => {
@@ -109,6 +201,10 @@ function App() {
               sortItems={sortItems}
               items={items}
               addToCart={addToCart}
+              filters={filters}
+              updateFilters={updateFilters}
+              resetFilters={resetFilters}
+              selectValue={selectValue}
             />
           }
         />
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -4,10 +4,20 @@ import styled from "styled-components";
 import ItemCard from "../components/ItemCard";
 
 function Shop(props) {
-  const { loadItems, sortItems, items, addToCart } = props;
+  const {
+    loadItems,
+    sortItems,
+    items,
+    addToCart,
+    filters,
+    updateFilters,
+    resetFilters,
+    selectValue,
+  } = props;
 
   useEffect(() => {
     loadItems();
+    resetFilters();
   }, []);
 
   return (
@@ -15,8 +25,7 @@ function Shop(props) {
       <SorterWrapper>
         <p>Sort</p>
         <StyledSelect
-          name="selectedSort"
-          defaultValue="sortDefault"
+          value={selectValue}
           onChange={(e) => {
             sortItems(e.target.value);
           }}
@@ -26,27 +35,131 @@ function Shop(props) {
           <option value="sortPriceHtoL">highest price</option>
         </StyledSelect>
       </SorterWrapper>
-      <ItemsGrid>
-        {items.map((item) => (
-          <ItemCard
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            rating={item.rating}
-            price={item.price}
-            img={item.img}
-            addToCart={addToCart}
-          />
-        ))}
-      </ItemsGrid>
+      <ItemsFiltersWrapper>
+        <FilterWrapper>
+          <FilterGroup>
+            <p>Sportswear for</p>
+            <div>
+              <input
+                type="checkbox"
+                id="gender1"
+                name="gender"
+                value="man"
+                checked={filters.gender.man}
+                onChange={(e) => updateFilters(e)}
+              />
+              <label htmlFor="gender1"> Men</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="gender2"
+                name="gender"
+                value="woman"
+                checked={filters.gender.woman}
+                onChange={(e) => updateFilters(e)}
+              />
+              <label htmlFor="gender2"> Women</label>
+            </div>
+          </FilterGroup>
+
+          <FilterGroup>
+            <p>Brand</p>
+            <div>
+              <input
+                type="checkbox"
+                id="brand1"
+                name="brand"
+                value="Hanes"
+                checked={filters.brand.Hanes}
+                onChange={(e) => updateFilters(e)}
+              />
+              <label htmlFor="brand1"> Hanes</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="brand2"
+                name="brand"
+                value="Champion"
+                checked={filters.brand.Champion}
+                onChange={(e) => updateFilters(e)}
+              />
+              <label htmlFor="brand2"> Champion</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="brand3"
+                name="brand"
+                value="Under_Armour"
+                checked={filters.brand.Under_Armour}
+                onChange={(e) => updateFilters(e)}
+              />
+              <label htmlFor="brand3"> Under Armour</label>
+            </div>
+          </FilterGroup>
+          <FilterGroup>
+            <p>Price</p>
+            <div>
+              <input
+                type="checkbox"
+                id="price1"
+                name="price"
+                value="upTo20"
+                checked={filters.price.upTo20}
+                onChange={(e) => updateFilters(e)}
+              />
+              <label htmlFor="price1"> Up to $20</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="price2"
+                name="price"
+                value="from20To25"
+                checked={filters.price.from20To25}
+                onChange={(e) => updateFilters(e)}
+              />
+              <label htmlFor="price2"> $20 to $25</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="price3"
+                name="price"
+                value="from25To30"
+                checked={filters.price.from25To30}
+                onChange={(e) => updateFilters(e)}
+              />
+              <label htmlFor="price3"> $25 to $30</label>
+            </div>
+          </FilterGroup>
+        </FilterWrapper>
+        {items.length > 0 ? (
+          <ItemsGrid>
+            {items.map((item) => (
+              <ItemCard
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                rating={item.rating}
+                price={item.price}
+                img={item.img}
+                addToCart={addToCart}
+              />
+            ))}
+          </ItemsGrid>
+        ) : (
+          <StyledP>No results found.</StyledP>
+        )}
+      </ItemsFiltersWrapper>
     </>
   );
 }
 
 const SorterWrapper = styled.div`
   display: flex;
-  width: 60%;
-  margin: 5rem auto 0;
   background-color: #f7f7f7;
   font-size: 14px;
   padding: 0.75rem 0;
@@ -59,19 +172,45 @@ const SorterWrapper = styled.div`
 const StyledSelect = styled.select`
   font: inherit;
   text-transform: capitalize;
-  padding: 0.5rem 0.75rem;
+  padding: 0.15rem 0.25rem;
   margin-right: 2rem;
   background-color: #fff;
   border-radius: 6px;
 `;
 
+const ItemsFiltersWrapper = styled.div`
+  display: flex;
+  padding: 4rem 6rem 2rem;
+`;
+
+const FilterWrapper = styled.div`
+  width: 15%;
+  display: flex;
+  flex-direction: column;
+  gap: 1.75rem;
+  font-size: 14px;
+`;
+
+const FilterGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  & p {
+    font-weight: 700;
+    margin-bottom: 4px;
+  }
+`;
+
 const ItemsGrid = styled.div`
   display: grid;
-  width: 60%;
-  padding: 4rem 0 8rem;
-  margin: 0 auto;
-  gap: 2.5rem;
-  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  width: 85%;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
+`;
+
+const StyledP = styled.p`
+  text-align: center;
 `;
 
 Shop.propTypes = {
@@ -79,6 +218,10 @@ Shop.propTypes = {
   loadItems: PropTypes.func,
   sortItems: PropTypes.func,
   addToCart: PropTypes.func,
+  filters: PropTypes.object,
+  updateFilters: PropTypes.func,
+  resetFilters: PropTypes.func,
+  selectValue: PropTypes.string,
 };
 
 export default Shop;
